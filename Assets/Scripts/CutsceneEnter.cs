bef67798aeee9f232a6bed5dadc0f2152e929888
Gianluca Pumoni <1603589+LucasTheAlchemist@users.noreply.github.com>
2021-02-02 13:22:08 +0000

    Movement and setting trigger

--- Assets/Scripts/CutsceneEnter.cs
+++ Assets/Scripts/CutsceneEnter.cs
@@ -6,9 +6,52 @@ public class CutsceneEnter : MonoBehaviour
 {
 
     public GameObject player;
+    public int scene = 5;
 
     void OnTriggerEnter(Collider other)
     {
         player.SetActive(false); //testing
+
+        List<Dictionary<string, object>> data = CSVReader.Read("test");
+
+        switch (scene)
+        {
+            case 5:
+                print("mp3 name " + data[5]["mp3 name"] + " " +
+                    "foreground name " + data[5]["foreground name"] + " " +
+                    "background name " + data[5]["background name"] + " " +
+                    "time " + data[5]["time"]);
+                break;
+            case 4:
+                print("mp3 name " + data[4]["mp3 name"] + " " +
+                    "foreground name " + data[4]["foreground name"] + " " +
+                    "background name " + data[4]["background name"] + " " +
+                    "time " + data[4]["time"]);
+                break;
+            case 3:
+                print("mp3 name " + data[3]["mp3 name"] + " " +
+                    "foreground name " + data[3]["foreground name"] + " " +
+                    "background name " + data[3]["background name"] + " " +
+                    "time " + data[3]["time"]);
+                break;
+            case 2:
+                print("mp3 name " + data[2]["mp3 name"] + " " +
+                     "foreground name " + data[2]["foreground name"] + " " +
+                     "background name " + data[2]["background name"] + " " +
+                     "time " + data[2]["time"]);
+                break;
+            case 1:
+                 print("mp3 name " + data[1]["mp3 name"] + " " +
+                    "foreground name " + data[1]["foreground name"] + " " +
+                    "background name " + data[1]["background name"] + " " +
+                    "time " + data[1]["time"]);
+                break;
+            default:
+                print("Incorrect ");
+                break;
+        }
+
+
+            
     }
 }
--- Assets/Scripts/ReadData.cs
+++ Assets/Scripts/ReadData.cs
@@ -4,16 +4,5 @@ using UnityEngine;
 
 public class ReadData : MonoBehaviour
 {
-    void Awake()
-    {
-        List<Dictionary<string, object>> data = CSVReader.Read("test");
 
-        for (var i = 0; i < data.Count; i++)
-        {
-            print("mp3 name " + data[i]["mp3 name"] + " " +
-                   "foreground name " + data[i]["foreground name"] + " " +
-                   "background name " + data[i]["background name"] + " " +
-                   "time " + data[i]["time"]);
-        }
-    }
 }
