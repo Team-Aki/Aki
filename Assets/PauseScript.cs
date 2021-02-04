f9cdbc7038113c2aab801a5a9928c5b9d26509bc
James Muirhead <60221059+JamesMuirhead@users.noreply.github.com>
2021-02-04 14:15:57 +0000

    Pause menu does stuff

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject panel;
+    private bool gameIsPaused;
+
+    void Start()
+    {
+        panel.SetActive(false);
+        gameIsPaused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                resume();
+            }
+
+            else
+            {
+                pause();
+            }
+        }
+    }
+
+    public void resume()
+    {
+        panel.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void pause()
+    {
+        panel.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
