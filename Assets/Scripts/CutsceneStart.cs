132c46621713d9420e8b3d04ecc2c7c4e172a902
Gianluca Pumoni <1603589+LucasTheAlchemist@users.noreply.github.com>
2021-02-04 15:21:36 +0000

    Parallax effect
    on cutscene images

--- Assets/Scripts/CutsceneStart.cs
+++ Assets/Scripts/CutsceneStart.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,28 +10,44 @@ public class CutsceneStart : MonoBehaviour
     public Image[] fore;
     public Image[] back;
 
+    private CanvasGroup fadeAlpha;
+
     Fader fader;
 
     [SerializeField] float fadeOutTime = 3f;
     [SerializeField] float fadeInTime = 3f;
     [SerializeField] float fadeWaitTime = 3f;
 
+    private Vector3 foreSpeed = new Vector3(10, 0);
+    private Vector3 backSpeed = new Vector3(5f, 0);
+
+    bool isPlaying;
+
     // Start is called before the first frame update
     void Start()
     {
 
+        fadeAlpha = GetComponentInChildren<CanvasGroup>();
+
+        isPlaying = false;
         //sound = GetComponent<Sound>();
+
         fader = FindObjectOfType<Fader>();
 
         for (int i = 0; i < fore.Length; i++)
         {
             fore[i].GetComponentInChildren<Image>();
+            fore[i].transform.localPosition = new Vector3(0, 0, 0);
+            fore[i].transform.localPosition = new Vector3(0, 0, 0);
             fore[i].enabled = false;
+
         }
         
         for (int i = 0; i < back.Length; i++)
         {
             back[i].GetComponentInChildren<Image>();
+            back[i].transform.localPosition = new Vector3(0, 0, 0);
+            back[i].transform.localPosition = new Vector3(0, 0, 0);
             back[i].enabled = false;
         }
     }
@@ -38,13 +55,10 @@ public class CutsceneStart : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         StartCoroutine(Transition());
-
-      
     }
 
     private IEnumerator Transition()
     {
-
         //cutscene.PlaySound(soundName);
         GetComponent<Collider>().enabled = false; //disable colliders as we won't need to play cutscene again
         yield return fader.FadeOut(fadeOutTime);
@@ -67,11 +81,15 @@ public class CutsceneStart : MonoBehaviour
         for (int i = 0; i < fore.Length; i++)
         {
             back[i].enabled = true;
+            //ScrollImage();
+            isPlaying = true;
         }
 
         for (int i = 0; i < back.Length; i++)
         {
             fore[i].enabled = true;
+            isPlaying = true;
+            //ScrollImage();
         }
     }
 
@@ -80,17 +98,35 @@ public class CutsceneStart : MonoBehaviour
         for (int i = 0; i < fore.Length; i++)
         {
             back[i].enabled = false;
+            isPlaying = false;
         }
 
         for (int i = 0; i < back.Length; i++)
         {
             fore[i].enabled = false;
+            isPlaying = false;
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isPlaying)
+            ScrollImage();
+    }
+
+    private void ScrollImage()
+    {
+        for (int i = 0; i < fore.Length; i++)
+        {
+
+            fore[i].transform.localPosition += foreSpeed * Time.deltaTime; //moves foreground
+
+        }
+
+        for (int i = 0; i < back.Length; i++)
+        {
+            back[i].transform.localPosition += backSpeed * Time.deltaTime; //moves background
+        }
     }
 }
