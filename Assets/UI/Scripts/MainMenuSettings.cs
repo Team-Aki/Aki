eaf3db4c2c3761121e33f71011703897793459de
James Muirhead <60221059+JamesMuirhead@users.noreply.github.com>
2021-02-04 17:15:26 +0000

    Setting menu added... UI done???

---
+++ Assets/UI/Scripts/MainMenuSettings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSettings : MonoBehaviour
+{
+
+    public GameObject title;
+    public GameObject mainMenu;
+    public GameObject settings;
+
+    void Start()
+    {
+        title.SetActive(true);
+        mainMenu.SetActive(true);
+        settings.SetActive(false);
+    }
+
+    public void openSettings()
+    {
+        title.SetActive(false);
+        mainMenu.SetActive(false);
+        settings.SetActive(true);
+    }
+
+    public void backToMenu()
+    {
+        title.SetActive(true);
+        mainMenu.SetActive(true);
+        settings.SetActive(false);
+    }
+}
--- Assets/UI/Scripts/PauseScript.cs
+++ Assets/UI/Scripts/PauseScript.cs
@@ -5,11 +5,16 @@ using UnityEngine.UI;
 
 public class PauseScript : MonoBehaviour
 {
+    public GameObject pauseFrame;
+    public GameObject settingsFrame;
     public GameObject panel;
+
     private bool gameIsPaused;
 
     void Start()
     {
+        pauseFrame.SetActive(false);
+        settingsFrame.SetActive(false);
         panel.SetActive(false);
         gameIsPaused = false;
     }
@@ -32,15 +37,27 @@ public class PauseScript : MonoBehaviour
 
     public void resume()
     {
+        pauseFrame.SetActive(false);
+        settingsFrame.SetActive(false);
         panel.SetActive(false);
+
         Time.timeScale = 1f;
         gameIsPaused = false;
     }
 
-    void pause()
+    public void pause()
     {
+        pauseFrame.SetActive(true);
+        settingsFrame.SetActive(false);
         panel.SetActive(true);
+
         Time.timeScale = 0f;
         gameIsPaused = true;
+    }
+
+    public void settings()
+    {
+        pauseFrame.SetActive(false);
+        settingsFrame.SetActive(true);
     }
 }
