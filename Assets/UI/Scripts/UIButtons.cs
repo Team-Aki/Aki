861886a83efd8ebdfb42a8f784a5aed5fd59a3c3
James Muirhead <60221059+JamesMuirhead@users.noreply.github.com>
2021-02-04 16:07:32 +0000

    Main menu buttons do stuff

---
+++ Assets/UI/Scripts/UIButtons.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour
+{
+    public void playGame()
+    {
+        //load whatever scene is the main game
+        Debug.Log("Play Game");
+    }
+
+    public void quitToOS()
+    {
+        Application.Quit();
+    }
+
+    public void quitToMenu()
+    {
+        //load main menu scene
+        Debug.Log("Load Main Menu");
+    }
+
+}
