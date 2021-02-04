f2118f676ffe78668a704b84ba96c303d9b86fa1
Gianluca Pumoni <1603589+LucasTheAlchemist@users.noreply.github.com>
2021-02-04 14:27:14 +0000

    Basic cutscene working
    yippee ki yay mf

---
+++ Assets/Scripts/CutsceneStart.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutsceneStart : MonoBehaviour
+{
+    //public Sound sound;
+    public Image[] fore;
+    public Image[] back;
+
+    Fader fader;
+
+    [SerializeField] float fadeOutTime = 3f;
+    [SerializeField] float fadeInTime = 3f;
+    [SerializeField] float fadeWaitTime = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //sound = GetComponent<Sound>();
+        fader = FindObjectOfType<Fader>();
+
+        for (int i = 0; i < fore.Length; i++)
+        {
+            fore[i].GetComponentInChildren<Image>();
+            fore[i].enabled = false;
+        }
+        
+        for (int i = 0; i < back.Length; i++)
+        {
+            back[i].GetComponentInChildren<Image>();
+            back[i].enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(Transition());
+
+      
+    }
+
+    private IEnumerator Transition()
+    {
+
+        //cutscene.PlaySound(soundName);
+        GetComponent<Collider>().enabled = false; //disable colliders as we won't need to play cutscene again
+        yield return fader.FadeOut(fadeOutTime);
+
+        //cutscene.PlayImage(imageName);
+
+        EnableImage();
+
+        // sceneImage.TryAdvance();
+
+        yield return new WaitForSeconds(fadeWaitTime);
+
+        DisableImage();
+
+        yield return fader.FadeIn(fadeInTime);
+    }
+
+    private void EnableImage()
+    {
+        for (int i = 0; i < fore.Length; i++)
+        {
+            back[i].enabled = true;
+        }
+
+        for (int i = 0; i < back.Length; i++)
+        {
+            fore[i].enabled = true;
+        }
+    }
+
+    private void DisableImage()
+    {
+        for (int i = 0; i < fore.Length; i++)
+        {
+            back[i].enabled = false;
+        }
+
+        for (int i = 0; i < back.Length; i++)
+        {
+            fore[i].enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
