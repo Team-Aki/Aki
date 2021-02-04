1efeee4ef198893d6e4682d28b18addb9bd65ae5
Gianluca Pumoni <1603589+LucasTheAlchemist@users.noreply.github.com>
2021-02-04 17:50:12 +0000

    Free look camera added
    not working atm

---
+++ Assets/Scripts/FreeLook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineFreeLook))]
+public class FreeLook : MonoBehaviour
+{
+    private CinemachineFreeLook freeLookCam;
+    // Use this for initialization
+    void Start()
+    {
+        freeLookCam = GetComponent<CinemachineFreeLook>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        freeLookCam.m_XAxis.Value = Input.GetAxis("Controller Right Stick X"); //Right Stick X
+        freeLookCam.m_YAxis.Value = Input.GetAxis("Controller Right Stick Y"); //Right Stick Y
+    }
+}
