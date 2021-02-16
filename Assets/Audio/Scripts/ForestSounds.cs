94cbdab643b38d872f18f1daf8500fa1d065a852
James Muirhead <60221059+JamesMuirhead@users.noreply.github.com>
2021-02-16 15:10:06 +0000

    Added sound mixer

---
+++ Assets/Audio/Scripts/ForestSounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestSounds : MonoBehaviour
+{
+    public Transform player;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        audioSource.transform.position = player.position;
+    }
+}
