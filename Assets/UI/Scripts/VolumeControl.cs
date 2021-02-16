2dde3cbd7d692f311449373babe42777bdb1638e
James Muirhead <60221059+JamesMuirhead@users.noreply.github.com>
2021-02-16 16:34:05 +0000

    Mixer attached to UI

---
+++ Assets/UI/Scripts/VolumeControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    public AudioMixer mixer1;
+    public Slider slider1;
+    
+    public void enviromentChange()
+    {
+        mixer1.SetFloat("Enviroment Volume", slider1.value);
+    }
+}
