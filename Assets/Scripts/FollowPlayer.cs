d3ed5347e0f2b5f7f6648aefaf0da8e81a6d9a44
Gianluca Pumoni <1603589+LucasTheAlchemist@users.noreply.github.com>
2021-02-16 16:28:47 +0000

    Basic AI working

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -8,11 +8,12 @@ public class FollowPlayer : MonoBehaviour
 
     public Transform player;
     public NavMeshAgent puppy;
+    Vector3 conversion;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //conversion = player.TransformPoint(player.parent.position);
     }
 
     // Update is called once per frame
