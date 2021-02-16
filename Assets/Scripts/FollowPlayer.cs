7f12b2aec80145bacc75cd1c4fc6f9203b7bb81c
Gianluca Pumoni <1603589+LucasTheAlchemist@users.noreply.github.com>
2021-02-16 15:19:30 +0000

    Basic AI setup (NOT WORKING)

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public Transform player;
+    public NavMeshAgent puppy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        puppy.SetDestination(player.position);
+    }
+}
