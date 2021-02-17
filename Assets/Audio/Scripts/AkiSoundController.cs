ddbbf0d16531cd461a275f95c1ab76a25c6e9601
James Muirhead <60221059+JamesMuirhead@users.noreply.github.com>
2021-02-17 15:36:29 +0000

    SFX controller added

---
+++ Assets/Audio/Scripts/AkiSoundController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AkiSoundController : MonoBehaviour
+{
+    public AudioClip[] akiSounds;
+    public AudioSource audioSource;
+
+    public void playWalk()
+    {
+        if(audioSource.clip != akiSounds[0])
+        {
+            audioSource.clip = akiSounds[0];
+            audioSource.Play();
+        }
+
+        else
+        {
+            audioSource.UnPause();
+        }
+    }
+
+    public void playRun()
+    {
+        if (audioSource.clip != akiSounds[1])
+        {
+            audioSource.clip = akiSounds[1];
+            audioSource.Play();
+        }
+
+        else
+        {
+            audioSource.UnPause();
+        }
+    }
+
+    public void playSniff()
+    {
+        if (audioSource.clip != akiSounds[2])
+        {
+            audioSource.clip = akiSounds[2];
+            audioSource.Play();
+        }
+
+        else
+        {
+            audioSource.UnPause();
+        }
+    }
+
+    public void pauseSFX()
+    {
+        audioSource.Pause();
+    }
+}
--- Assets/UI/Scripts/VolumeControl.cs
+++ Assets/UI/Scripts/VolumeControl.cs
@@ -8,9 +8,16 @@ public class VolumeControl : MonoBehaviour
 {
     public AudioMixer mixer1;
     public Slider slider1;
-    
+    public AudioMixer mixer2;
+    public Slider slider2;
+
     public void enviromentChange()
     {
         mixer1.SetFloat("Enviroment Volume", slider1.value);
+    }
+
+    public void SFXChange()
+    {
+        mixer2.SetFloat("SFX Volume", slider2.value);
     }
 }
