681a34bb1b4a7515ca51eac04273b2b34a062ab4
Morgan Skillicorn <hello@morgan.games>
2021-02-22 16:22:18 +0000

    Added spirit vfx and fixed main menu

---
+++ Assets/VFX_Toggle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VFX_Toggle : MonoBehaviour
+{
+    public bool SpiritOn;
+
+    public GameObject VFXLiving;
+    public GameObject VFXSpirit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SpiritOn)
+        {
+            VFXLiving.gameObject.SetActive(false);
+            VFXSpirit.gameObject.SetActive(true);
+        }
+        else
+        {
+            VFXLiving.gameObject.SetActive(true);
+            VFXSpirit.gameObject.SetActive(false);
+        }
+    }
+}
