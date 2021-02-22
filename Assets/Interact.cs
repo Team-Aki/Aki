bd59241bc252d3b93fa0d3e4170908eca336aa1c
Gianluca Pumoni <1603589+LucasTheAlchemist@users.noreply.github.com>
2021-02-22 15:14:10 +0000

    Added working Interaction

---
+++ Assets/Interact.cs
@@ -0,0 +1,46 @@
+﻿using Haven.PathPainter;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interact : MonoBehaviour
+{
+    [SerializeField] bool inRange;
+    [SerializeField] KeyCode interact;
+    [SerializeField] UnityEvent interactAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inRange)
+        {
+            if(Input.GetKeyDown(interact))
+            {
+                interactAction.Invoke();
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Cutscene"))
+        {
+            inRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cutscene"))
+        {
+            inRange = false;
+        }
+    }
+}
