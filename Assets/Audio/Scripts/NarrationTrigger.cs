c622a0f0ba237471ddbc32e7b67c2dbf02b43aa5
James Muirhead <60221059+JamesMuirhead@users.noreply.github.com>
2021-02-23 16:35:56 +0000

    Built narration trigger

---
+++ Assets/Audio/Scripts/NarrationTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarrationTrigger : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private bool alreadyPlayed = false;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(alreadyPlayed == false)
+        {
+            alreadyPlayed = true;
+            audioSource.Play();
+        }
+    }
+}
