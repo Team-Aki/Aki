3fe75690611cc9a7087a779f9650d0d03ae9aecb
James Muirhead <60221059+JamesMuirhead@users.noreply.github.com>
2021-02-23 13:22:56 +0000

    Volume control UI added to main menu

--- Assets/UI/Scripts/VolumeControl.cs
+++ Assets/UI/Scripts/VolumeControl.cs
@@ -11,6 +11,36 @@ public class VolumeControl : MonoBehaviour
     public AudioMixer mixer2;
     public Slider slider2;
 
+    private void Start()
+    {
+        float volume1;
+        float volume2;
+
+        bool result = mixer1.GetFloat("Enviroment Volume", out volume1);
+
+        if (result)
+        {
+            slider1.value = volume1;
+        }
+
+        else
+        {
+            slider1.value = 0f;
+        }
+
+        result = mixer2.GetFloat("SFX Volume", out volume2);
+
+        if (result)
+        {
+            slider2.value = volume2;
+        }
+
+        else
+        {
+            slider2.value = 0f;
+        }
+    }
+
     public void enviromentChange()
     {
         mixer1.SetFloat("Enviroment Volume", slider1.value);
